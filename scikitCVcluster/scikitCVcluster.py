fadf8c87d15b67a9712239063eb7f554cf3fcc0d
chausler <chausler@gmail.com>
2012-12-18 19:37:25 +0100

    h

--- scikitCVcluster/scikitCVcluster.py
+++ scikitCVcluster/scikitCVcluster.py
@@ -43,14 +43,12 @@ class scikitCVcluster():
 
     def CV(self, clf, X, y, folds=5, clf_args={}, clf_fit_args={},
            clf_pred_args={}, return_coefs=False):
-        print clf, X, y, folds
+
         cv = KFold(len(X), k=folds, indices=True)#, shuffle=True)
         self.dview.push({'X': X, 'y': y, 'clf': clf, 'clf_args': clf_args,
                          'fit_args': clf_fit_args, 'pred_args': clf_pred_args,
                          'return_coefs' : return_coefs})
         pred = []
-        print self.dview['clf']
-        print self.dview['X']
         try:
             pred = self.dview.map(classify, cv)
         except RemoteError as e:
@@ -77,4 +75,5 @@ if __name__ == "__main__":
     cv = scikitCVcluster()
     cc = neighbors.KNeighborsClassifier
     preds, _ = cv.CV(cc, iris.data, iris.target)
+    print preds
     print 'Accuracy: %.2f' % (np.sum(preds == iris.target) / (len(iris.target) * 1.))
