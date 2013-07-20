7963be1ab5060b5011ed42e15fa46df298620444
chausler <chausler@gmail.com>
2013-07-20 13:19:17 +0200

    Working now. added ipython interactive to classify function

--- scikitCVcluster/scikitCVcluster.py
+++ scikitCVcluster/scikitCVcluster.py
@@ -1,15 +1,19 @@
-import sys
-sys.path.append('..')
-#sys.path = ['/home/chris/programs/aa_scikits/scikit-learn'] + sys.path
+"""
+    Handles cross validation training of a classifier in parallel
+    using Ipython.parallel
+    requires libraries sklearn, numpy and Ipython
+    @author: Chris Hausler
+"""
+
 from sklearn.cross_validation import KFold
 import numpy as np
 try:
     from IPython.parallel import Client
     from IPython.parallel.error import RemoteError
+    from IPython.parallel.util import interactive
 except:
     Client = None
     print 'Failed to find IPython.parallel - No parallel processing available'
-
 
 
 print 'hooraz' if Client else 'blag'
@@ -18,29 +22,45 @@ dview = rc[:]
 print '%d engines found' % len(rc.ids)
 
 
+@interactive
 def classify(cv):
+    """
+    This method initialises the classifier and trains it on  the training data
+    @return: prediction from the test data and the classifier coefficients 
+        if required
+    """
     train = cv[0]
     test = cv[1]
-    gg = X[train]
     regr = clf(**clf_args)
-    regr.fit(X[train], y[train])
+    regr.fit(X[train], y[train], **clf_fit_args)
     coef = None
     if return_coefs:
         try:
             coef = regr.coef_
         except:
             pass
-    pred = regr.predict(X[test])
+    pred = regr.predict(X[test], **clf_pred_args)
     return (pred, coef)
-        #return (train, test)
 
 
-def CV(clf, X, y, folds=5, clf_args={}, clf_fit_args={},
+def CV(clf, X, y, folds=5, shuffle=True, clf_args={}, clf_fit_args={},
        clf_pred_args={}, return_coefs=False):
-
-    cv = KFold(len(X), k=folds, indices=True)#, shuffle=True)
+    """
+    @param clf: the classifier to use, must have methods fit and predict
+    @param X: the training data. [samples, dimensions]
+    @param y: the target data. [targets]
+    @param folds: how many folds to use in the cross validation
+    @param shuffle: whether or not to shuffle the samples for xval
+    @param clf_args: arguments for initialising the classifier
+    @param clf_fit_args: arguments for training the classifier
+    @param clf_pred_args: arguments for predicting with the classifier
+    @param return_coefs: bool, whether or not to return classifier coefficients
+        the classifier must have the attribute coef_ for this
+    @return: numpy arrays of the predictions and coefficients
+    """
+    cv = KFold(len(X), k=folds, indices=True, shuffle=shuffle)
     dview.push({'X': X, 'y': y, 'clf': clf, 'clf_args': clf_args,
-                     'fit_args': clf_fit_args, 'pred_args': clf_pred_args,
+                     'clf_fit_args': clf_fit_args, 'clf_pred_args': clf_pred_args,
                      'return_coefs': return_coefs})
     pred = []
     try:
--- scikitCVcluster/test.py
+++ scikitCVcluster/test.py
@@ -1,12 +1,12 @@
 from scikitCVcluster import CV
-from simple_test import simple_test
 from sklearn import neighbors
 from sklearn import datasets
 import numpy as np
 
-simple_test()
+
 iris = datasets.load_iris()
 cc = neighbors.KNeighborsClassifier
 preds, _ = CV(cc, iris.data, iris.target)
 print preds
-print 'Accuracy: %.2f' % (np.sum(preds == iris.target) / (len(iris.target) * 1.))
+print 'Accuracy: %.2f' % (np.sum(preds == iris.target)
+                          / (len(iris.target) * 1.))
